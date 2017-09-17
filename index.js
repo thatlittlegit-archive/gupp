64ac87a925ae054060757d62cd4827a891284513
thatlittlegit <wapidstyle@live.ca>
2017-09-17 16:30:11 -0400

    Add linting/editorconfig and fix errors

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ import {EventEmitter2} from 'eventemitter2';
 export const emitter = new EventEmitter2();
 
 const gupp = {
-		task(name, cb) {
-				emitter.on(`tasks.${name}`, cb);
-		}
+	task(name, cb) {
+		emitter.on(`tasks.${name}`, cb);
+	}
 };
 
 export default gupp;
